f160ff6843fc97580bfbbacb0ebc583e42f16007
Vitor Bozi <vitorbozi228@gmail.com>
2023-03-05 10:50:52 -0300

    versao pagamento

---
+++ src/Pagamento.java
@@ -0,0 +1,25 @@
+public class Pagamento {
+
+    private int tipoPagamento;
+
+    public Pagamento (int Pagamento){
+        this.tipoPagamento = tipoPagamento;
+       
+    }
+    
+    public int getTipoPagamento() {
+        return tipoPagamento;
+    }
+
+    public void setTipoPagamento(int tipoPagamento) {
+        this.tipoPagamento = tipoPagamento;
+    }
+    
+    public void selecionarTipoPagamento(int tipoPagamento){
+
+    }
+
+    public void realizarPagamento(Pagamento pagamento){
+        
+    }
+}
