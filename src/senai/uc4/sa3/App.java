2bd4cca159efb3f218142a63131a57fe14d226b7
Lilflowerpt <108307575+Lilflowerpt@users.noreply.github.com>
2023-03-04 21:47:10 -0300

    testeDeClass

--- src/senai/uc4/sa3/App.java
+++ src/senai/uc4/sa3/App.java
@@ -3,7 +3,10 @@ package senai.uc4.sa3;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Produto produto = new Produto("Arroz",15,1);
+
     }
+
+
 
 }
