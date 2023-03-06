aba594564984e3c0ef2d518560ac9ad022dd0141
Vitor Bozi <vitorbozi228@gmail.com>
2023-03-06 20:21:27 -0300

    funcoes da classe pagamento feitas

--- src/senai/uc4/sa3/App.java
+++ src/senai/uc4/sa3/App.java
@@ -4,6 +4,12 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         Produto produto = new Produto("Arroz",15,1);
+
+        Pagamento p1 = new Pagamento(1);
+
+        p1.selecionarTipoPagamento(4);
+      //  p1.realizarPagamento(p1);
+        
     }
 
 
--- src/senai/uc4/sa3/Pagamento.java
+++ src/senai/uc4/sa3/Pagamento.java
@@ -1,4 +1,5 @@
 package senai.uc4.sa3;
+import java.util.Scanner;
 public class Pagamento {
 
     private int tipoPagamento;
@@ -16,11 +17,36 @@ public class Pagamento {
         this.tipoPagamento = tipoPagamento;
     }
     
-    public void selecionarTipoPagamento(int tipoPagamento){
+    public void selecionarTipoPagamento(int pag){
+        this.tipoPagamento = pag;
+        switch(pag){
+            case 1:
+            System.out.println("1 - Dinheiro. ");
+            break;
+
+            case 2: 
+            System.out.println("2 - Cheque. ");
+            break;
+
+            case 3:
+            System.out.println("3 - Cartão de débito. ");
+            break;
+
+            case 4:
+            System.out.println("4 - Cartão de crédito. ");
+            break;
+
+            case 5:
+            System.out.println("5 - Pix. ");
+            break;
+
+            default : System.out.println("Nenhum meio de pagamento selecionado!");
+            
+        }
 
     }
-
+      
     public void realizarPagamento(Pagamento pagamento){
-
+        System.out.println("Pagamento realizado com a opção " +tipoPagamento);
     }
 }
